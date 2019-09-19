86cac488a85c16a1a47ea3a396d2a8567955f5d8
Lorenzo Billi <lorenzoaebilli@gmail.com>
2019-09-19 07:52:51 +0200

    Version 0.1.0

--- Espresso/Espresso.WPF/Properties/AssemblyInfo.cs
+++ Espresso/Espresso.WPF/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Espresso.WPF")]
+[assembly: AssemblyTitle("Espresso")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Espresso.WPF")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Espresso")]
+[assembly: AssemblyCopyright("Copyright ©  2018-2019 Lorenzo Billi")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]

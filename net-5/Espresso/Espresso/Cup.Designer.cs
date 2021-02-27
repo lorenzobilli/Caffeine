cea5c49fb6210aa46b067749d447995f9623faec
Lorenzo Billi <lorenzoaebilli@gmail.com>
2021-02-27 17:22:38 +0100

    Final code cleanup

--- net-5/Espresso/Espresso/Cup.Designer.cs
+++ net-5/Espresso/Espresso/Cup.Designer.cs
@@ -1,8 +1,41 @@
-﻿
+﻿/*
+ *	Project: Espresso
+ *	Author(s): Lorenzo Billi
+ *	
+ *	
+ *	The MIT License
+ *	
+ *	Copyright (c) 2018-2021 Lorenzo Billi
+ *	
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *	documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ *	the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ *	to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *	
+ *	The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ *	of the Software.
+ *	
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *	THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND	NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ *	CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *	IN THE SOFTWARE.
+ *	
+ *	
+ *	Espresso/Espresso/Cup.Designer.cs
+ *	
+ */
+
 namespace Espresso
 {
+	/// <summary>
+	/// Underlining code of the designer Cup class
+	/// </summary>
 	partial class Cup
 	{
+		/// <summary>
+		/// Determines if the (dummy) main form shall be visible.
+		/// </summary>
 		private readonly bool showForm = false;
 
 		/// <summary>
@@ -10,6 +43,10 @@ namespace Espresso
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 
+		/// <summary>
+		/// Hides or shows the (dummy) main form. By defaults, it hides the form on application startup.
+		/// </summary>
+		/// <param name="value">True if the form shall be visible, false if not.</param>
 		protected override void SetVisibleCore(bool value) => base.SetVisibleCore(showForm ? value : showForm);
 
 		/// <summary>
--- net-5/Espresso/Espresso/Cup.cs
+++ net-5/Espresso/Espresso/Cup.cs
@@ -1,23 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿/*
+ *	Project: Espresso
+ *	Author(s): Lorenzo Billi
+ *	
+ *	
+ *	The MIT License
+ *	
+ *	Copyright (c) 2018-2021 Lorenzo Billi
+ *	
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *	documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ *	the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ *	to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *	
+ *	The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ *	of the Software.
+ *	
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *	THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND	NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ *	CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *	IN THE SOFTWARE.
+ *	
+ *	
+ *	Espresso/Espresso/Cup.cs
+ *	
+ */
+
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Espresso
 {
+	/// <summary>
+	/// Main form class of the application, representing the tray icon and all associated items.
+	/// </summary>
 	public partial class Cup : Form
 	{
+		/// <summary>
+		/// Determines the size of the tray icon.
+		/// </summary>
 		private static readonly Size TRAY_ICON_SIZE = new(256, 256);
 
+		/// <summary>
+		/// Icon used when the application is in the disabled state.
+		/// </summary>
 		private readonly Icon disabledIcon = new(Icons.disabled, TRAY_ICON_SIZE);
 
+		/// <summary>
+		/// Icon used when the application is in the enabled state.
+		/// </summary>
 		private readonly Icon enabledIcon = new(Icons.enabled, TRAY_ICON_SIZE);
 
+		/// <summary>
+		/// Spawns the tray icon and associates the main events to the corresponding actions.
+		/// </summary>
 		public Cup()
 		{
 			InitializeComponent();

1c254b2816ba1a0641956689fbb4db81c61777d7
Lorenzo Billi <lorenzoaebilli@gmail.com>
2021-02-28 00:05:35 +0100

    Settings form linked to new context menu entry

---
+++ Espresso/Espresso/Settings.Designer.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Espresso
+{
+	partial class Settings
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
+			this.SuspendLayout();
+			// 
+			// Settings
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Settings";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Settings";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}

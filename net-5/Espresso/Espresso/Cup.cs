d20a9398e8386e24628545daffbd4bf96929e65a
Lorenzo Billi <lorenzoaebilli@gmail.com>
2021-02-27 17:03:58 +0100

    Coffee class linked to Cup form

--- net-5/Espresso/Espresso/Cup.cs
+++ net-5/Espresso/Espresso/Cup.cs
@@ -14,16 +14,53 @@ namespace Espresso
 	{
 		private static readonly Size TRAY_ICON_SIZE = new(256, 256);
 
-		private Icon disabledIcon = new(Icons.disabled, TRAY_ICON_SIZE);
+		private readonly Icon disabledIcon = new(Icons.disabled, TRAY_ICON_SIZE);
 
-		private Icon enabledIcon = new(Icons.enabled, TRAY_ICON_SIZE);
+		private readonly Icon enabledIcon = new(Icons.enabled, TRAY_ICON_SIZE);
 
 		public Cup()
 		{
 			InitializeComponent();
 
 			trayIcon.Icon = disabledIcon;
+			trayIcon.MouseDown += (sender, e) => FilterMouseInput(sender, e);
+
 			toggleMenuItem.Text = "Enable";
+		}
+
+		/// <summary>
+		/// Toggles between enabled and disabled application states.
+		/// </summary>
+		private void SetState()
+		{
+			if (Coffee.IsActive)
+			{
+				if (Coffee.Deactivate())
+				{
+					trayIcon.Icon = disabledIcon;
+					toggleMenuItem.Text = "Enable";
+				}
+			}
+			else
+			{
+				if (Coffee.Activate())
+				{
+					trayIcon.Icon = enabledIcon;
+					toggleMenuItem.Text = "Disable";
+				}
+			}
+		}
+
+		/// <summary>
+		/// Filters user mouse input, distinguishing between left and right click.
+		/// SetState() must be called only with a left mouse click.
+		/// </summary>
+		private void FilterMouseInput(object sender, MouseEventArgs e)
+		{
+			if (e.Button.Equals(MouseButtons.Left))
+			{
+				SetState();
+			}
 		}
 	}
 }

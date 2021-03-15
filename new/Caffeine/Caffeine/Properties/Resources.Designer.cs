27e06b3a2133c39edce779cb392f97f2df043640
Lorenzo Billi <lorenzoaebilli@gmail.com>
2021-03-15 11:38:21 +0100

    New project finalized

--- new/Caffeine/Caffeine/Properties/Resources.Designer.cs
+++ new/Caffeine/Caffeine/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Caffeine.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Espresso.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Caffeine.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
